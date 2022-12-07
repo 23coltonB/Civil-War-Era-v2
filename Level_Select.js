b8432b64c57b2223ee3f3f628f198b1cecc3546b
Colton Bennett <78919847+23coltonB@users.noreply.github.com>
2022-12-07 02:07:29 +0000

    Nerfed all characters, so that there is a higher chance of a different scene. Going to test crit chance, so that I can see if my new programming works

--- Level_Select.js
+++ Level_Select.js
@@ -3,6 +3,7 @@ function levelSelect(){
     $('#scenetext').html('Thank you so much for testing or running through my game. There is plenty more on the way including the second Chapter, graphics and so much more! Fill free to replay this as many times as you want, to see multipole perspectives, endings, ect.');
     $('#question').html('Please type "quitgame" into the answer bar to go to the beginning!');
   }else{
+    End = false;
     $('#scenetext').html('Demos kinda not working');
   }
 }
