2d580212d5b0aa7c0298366e3ba433733148786e
Colton Bennett <78919847+23coltonB@users.noreply.github.com>
2022-12-08 18:01:10 +0000

    Adjusted the crit system from X >= player.stat, to X <= player.stat, in addition to reworked the player stats.

--- 1.Investigate.js
+++ 1.Investigate.js
@@ -357,7 +357,7 @@ currentScene = 36;
   if (enemy > 0){
     if (player.race != 'elf'){
       if (x <= player.accuracyM && x <= player.strength){
-        if (x >= player.randomCrit){
+        if (x <= player.randomCrit){
           if (enemy != 1){
           $('#scenetext').html('You ready yourself for the oncomeing onslaught. You effortlessly parry the first assailent with your ' + player.twoWeapons + ', strikeing down the second one with your' + player.weapons + ' and dodging the third. To your surpise, you ended up knocking out one of the assailents when you parried. Good for you.');
           enemy = Number(enemy) - 2;
@@ -388,7 +388,7 @@ currentScene = 36;
     }else{
       if (x <= player.dodge && x <= player.speed){
         if (x <= player.accuracyR){
-          if (x >= player.randomCrit){
+          if (x <= player.randomCrit){
             $('#scenetext').html("You dive out of the way of your assailents, launching your arrows towards one of the enemies. You hear the satifying sound of an arrow hitting the target, but luckly for you, you got two of them! Make sure you don't get too cocky though....");
           enemy = Number(enemy) - 2;
           }else{
@@ -427,7 +427,7 @@ function scene37(){
 currentScene = 37;
     if (enemy > 0){
     if (x <= player.strength){
-      if (x >= player.randomCrit){
+      if (x <= player.randomCrit){
         $('#scenetext').html('You stand your ground, and prepare to defend yourself from the three attackers. You effectivly parry all the attacks from your assailents. In fact, you hit them so hard, you knocked out one cold on the ground.');
         enemy = Number(enemy) - 1;
       }else{
@@ -461,7 +461,7 @@ function scene38(){
 currentScene = 38;
   if (enemy > 0){
     if (x <= player.magic && x <= player.accuracyR){
-      if (x >= player.randomCrit){
+      if (x <= player.randomCrit){
         $('#scenetext').html('You throw your hands out towards the advancing assailent. With your staff, you slightly more powerfull than you are without it. <br> <br> "Fulgur Percutiens!" you shout despretly, hoping that it will work this time. Electrisity fills your veins, and a lightning bolt shoots out from your fingertips. It lands directly through one of the assailents, then, like a leaping rabbet, the bolt lept to the second one, shocking the secoond one as well.');
         enemy = Number(enemy) - 2;
       }else{
@@ -497,7 +497,7 @@ function scene39(){
   if (enemy > 0){
     if (player.race == 'elf'){
       if (x <= player.accuracyR){
-        if (x >= player.randomCrit){
+        if (x <= player.randomCrit){
           $('#scenetext').html('You fire an arrow into the advanceing group. Luckly for you, it hits one of them. To your surpise, it ricochets off of the first assailent, and into the second one. Congrats.');
           enemy = Number(enemy) - 2;
         }else{
@@ -513,7 +513,7 @@ function scene39(){
       }
     }else{
           if (x <= player.accuracyM){
-        if (x >= player.randomCrit){
+        if (x <= player.randomCrit){
           $('#scenetext').html('You charge head first into your assailents, screaming at them with your world renound battle cry. You effectivly use your ' + player.weapons + ' to incompacitate two of your assailents as you hop out of your assailents reach before they can effectivly hurt you. Congrats!');
           enemy = Number(enemy) - 2;
         }else{
